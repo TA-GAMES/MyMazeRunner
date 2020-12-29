ecda958e5bb9b64d0c05f527b1a742bfcd4b3ae3
TalAbed <47939585+TalAbed@users.noreply.github.com>
2020-12-29 19:02:30 +0200

    Add files via upload

---
+++ Assets/Scripts/GoToLevel2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToLevel2 : MonoBehaviour
+{
+
+    [SerializeField] string sceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ScoreScript.LevelScore == 200)
+        {
+            SceneManager.LoadScene(sceneName);
+            ScoreScript.LevelScore = 0;
+        }
+    }
+}
